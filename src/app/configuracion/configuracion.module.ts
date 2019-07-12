4745a90bf186d8a5da909a75b39416dea55cd845
Ricardo Rojas Peña <rjaspe0905@hotmail.com>
2019-07-12 15:09:01 -0500

    Se inserta un nuevo template HTML para la aplicación y se crea el primer componente de gestión de parámetros

---
+++ src/app/configuracion/configuracion.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ParametroComponent } from './parametro/parametro.component';
+
+@NgModule({
+  declarations: [ParametroComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfiguracionModule { }
